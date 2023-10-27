39e4e37b585ad617c4aa4784a366764334a32595
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2023-10-27 13:15:16 +0600

    add a hero register page to layout

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,11 +1,17 @@
 
 const Register = () => {
 
+  const handleRegister = event => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email, password);
+  }
 
   return (
     <div>
       <h2 className="text-3xl my-4">Please Register</h2>
-      <form className="my-4">
+      <form onSubmit={handleRegister} className="my-4">
         <input className="my-4 w-3/4 rounded-sm py-2 px-2" type="email" name="email" placeholder="Email Address" id="" />
         <br />
         <input className="my-4 w-3/4 rounded-sm py-2 px-2" type="password" name="password" placeholder="Password" id="" />
